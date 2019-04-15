6fc2696a0cc5f0aa0437c977c44ef18e20666ee6
Michael Agard <mdagard@gmail.com>
2019-04-15 16:58:40 -0400

    add HelpHeader for help button

---
+++ react-app/src/components/HelpHeader/HelpHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const HelpDiv = styled.div`
+  
+`;
+
+class HelpHeader extends Component {
+  render() {
+    return (
+      <p>Hello World!</p>
+    );
+  }
+}
+
+export default HelpHeader;
