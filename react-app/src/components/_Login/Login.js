325c636887e52fdb238bdad21347b29f98727f72
Michael Agard <mdagard@gmail.com>
2019-04-15 16:59:01 -0400

    add Login component to house login screen

---
+++ react-app/src/components/_Login/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import HelpHeader from '../HelpHeader/HelpHeader'
+class Login extends Component {
+  render() {
+    return (
+      <HelpHeader/>
+    );
+  }
+}
+
+export default Login;
