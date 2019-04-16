01c25c0c420172b86bfd8bb6d6a6a175d70f4fae
Michael Agard <mdagard@gmail.com>
2019-04-16 15:47:24 -0400

    add temp box shadow for help button

--- src/components/common/HelpButton.js
+++ src/components/common/HelpButton.js
@@ -15,6 +15,9 @@ const HelpLink = styled.a`
 	:visited{
 		color: white;
 	}
+	-moz-box-shadow:    -4px 2px 10px 1px black;
+  -webkit-box-shadow: -4px 2px 10px 1px black;
+  box-shadow:         -4px 2px 10px 1px black;
 `;
 
 class HelpHeader extends Component {
