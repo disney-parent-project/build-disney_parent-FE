0bbb9e01e5136a5d252bc2f9704aa1a3a66e3378
Michael Agard <mdagard@gmail.com>
2019-04-16 13:13:12 -0400

    remove hello function and route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
-import Login from './components/Login';
+import Login from './components/login/Login';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-
-const Hello = () => {
-  return (
-    <p>Hello</p>
-  )
-}
 
 class App extends Component {
   render() {
@@ -14,7 +8,6 @@ class App extends Component {
       <Router>
         <Switch>
           <Route path="/" component={Login} exact={true} />
-          <Route path="/hello/" component={Hello} exact={true} />
           <Redirect to="/" />
         </Switch>
       </Router>
