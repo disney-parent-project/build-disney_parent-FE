0e3db61e367d7cccd58c932e220de1c43b23c5b8
Michael Agard <mdagard@gmail.com>
2019-04-17 18:53:53 -0400

    add route for signup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import LP from './components/common/LP';
 import Login from './components/login/Login';
+import Signup from './components/signup/Signup';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
         <Switch>
           <Route path="/" component={LP} exact={true} />
           <Route path="/login" component={Login} exact={true} />
+          <Route path="/signup" component={Signup} exact={true} />
           <Redirect to="/" />
         </Switch>
       </Router>
