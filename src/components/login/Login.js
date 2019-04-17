6d4645a40259155a888939dee505cc6ec6dd9520
Michael Agard <mdagard@gmail.com>
2019-04-17 11:32:37 -0400

    restructure login.js

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -4,6 +4,46 @@ import logo from '../../assets/logo.png';
 import HelpButton from '../common/HelpButton';
 import login_background from '../../assets/login_bg.jpg';
 
+class Login extends Component {
+  render() {
+
+    function Logo() {
+      // Import result is the URL of your image
+      return <img src={logo} alt="Logo" />;
+    }
+
+    return (
+      <LoginContainer>
+        <HelpButton/>
+        <LogoTitleWrapper>
+          <LogoContainer>
+          <Logo />
+          </LogoContainer>
+          Disney Parent App
+        </LogoTitleWrapper>
+        <SignInButtonWrapper>
+          <SignInButton>
+            Sign In
+          </SignInButton>
+          <SecondParentSignInButton>
+            Second Parent Sign In
+          </SecondParentSignInButton>
+        </SignInButtonWrapper>
+        <SignUpButtonWrapper>
+          <SignUpButton>
+            No Account? Sign up Here
+          </SignUpButton>
+          <BusinessSignUpButton>
+            I'm A Business Owner
+          </BusinessSignUpButton>
+        </SignUpButtonWrapper>
+      </LoginContainer>
+    );
+  }
+}
+
+export default Login;
+
 const LoginContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -46,13 +86,14 @@ const Button = styled.a`
 	color: white;
 	text-decoration: none;
 	font-family: 'Merienda', cursive;
-	text-align: center;
+  text-align: center;
+  border-radius: 3px;
 	:visited{
 		color: white;
 	}
-	-moz-box-shadow:    -4px 2px 10px 1px black;
-  -webkit-box-shadow: -4px 2px 10px 1px black;
-  box-shadow:         -4px 2px 10px 1px black;
+	-moz-box-shadow:    2px 1px 6px black;
+  -webkit-box-shadow: 2px 1px 6px black;
+  box-shadow:         2px 1px 6px black;
 `;
 
 const SignInButtonWrapper = styled.div`
@@ -68,6 +109,7 @@ const SignInButtonWrapper = styled.div`
 const SignInButton = styled(Button)`
   height: 37px;
   width: 50%;
+  
 `;
 
 const SecondParentSignInButton = styled(Button)`
@@ -85,41 +127,3 @@ const SignUpButton = styled(Button)`
 const BusinessSignUpButton = styled(Button)`
   
 `;
-
-class Login extends Component {
-  render() {
-    function Logo() {
-      // Import result is the URL of your image
-      return <img src={logo} alt="Logo" />;
-    }
-    return (
-      <LoginContainer>
-        <HelpButton/>
-        <LogoTitleWrapper>
-          <LogoContainer>
-          <Logo />
-          </LogoContainer>
-          Disney Parent App
-        </LogoTitleWrapper>
-        {/* <SignInButtonWrapper>
-          <SignInButton>
-            Sign In
-          </SignInButton>
-          <SecondParentSignInButton>
-            Second Parent Sign In
-          </SecondParentSignInButton>
-        </SignInButtonWrapper>
-        <SignUpButtonWrapper>
-          <SignUpButton>
-            No Account? Sign up Here
-          </SignUpButton>
-          <BusinessSignUpButton>
-            I'm A Business Owner
-          </BusinessSignUpButton>
-        </SignUpButtonWrapper> */}
-      </LoginContainer>
-    );
-  }
-}
-
-export default Login;
