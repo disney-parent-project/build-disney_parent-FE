45576ac2cf7c46e5af61b45478792d46353af890
Michael Agard <mdagard@gmail.com>
2019-04-17 12:47:28 -0400

    changed Login.js to LP.js and styled

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,129 +1,67 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import logo from '../../assets/logo.png';
-import HelpButton from '../common/HelpButton';
-import login_background from '../../assets/login_bg.jpg';
 
-class Login extends Component {
+export default class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email : '',
+      password: ''
+    };
+  }
+
+  handleInputChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    // API call to BE to login to the app
+    fetch('https://disney-parent-api.herokuapp.com/api/parents/login', {
+      method: 'POST',
+      body: JSON.stringify(this.state),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => {
+      if (res.status === 200) {
+        this.props.history.push('/');
+      } else {
+        const error = new Error(res.error);
+        throw error;
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      alert('Error logging in please try again');
+    });
+  }
+
   render() {
-
-    function Logo() {
-      // Import result is the URL of your image
-      return <img src={logo} alt="Logo" />;
-    }
-
     return (
-      <LoginContainer>
-        <HelpButton/>
-        <LogoTitleWrapper>
-          <LogoContainer>
-          <Logo />
-          </LogoContainer>
-          Disney Parent App
-        </LogoTitleWrapper>
-        <SignInButtonWrapper>
-          <SignInButton>
-            Sign In
-          </SignInButton>
-          <SecondParentSignInButton>
-            Second Parent Sign In
-          </SecondParentSignInButton>
-        </SignInButtonWrapper>
-        <SignUpButtonWrapper>
-          <SignUpButton>
-            No Account? Sign up Here
-          </SignUpButton>
-          <BusinessSignUpButton>
-            I'm A Business Owner
-          </BusinessSignUpButton>
-        </SignUpButtonWrapper>
-      </LoginContainer>
+      <form onSubmit={this.onSubmit}>
+        <h1>Login Below!</h1>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          value={this.state.email}
+          onChange={this.handleInputChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter password"
+          value={this.state.password}
+          onChange={this.handleInputChange}
+          required
+        />
+       <input type="submit" value="Submit"/>
+      </form>
     );
   }
 }
-
-export default Login;
-
-const LoginContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 16px 8px;
-  height: 100vh;
-  ::after {
-    content: "";
-    background:
-      linear-gradient(
-        rgba(0, 0, 0, 0.54), 
-        rgba(0, 0, 0, 0.54)
-      ),
-      url('${login_background}');
-    background-size: cover;
-    background-position: center;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    position: absolute;
-    z-index: -1;   
-  };
-`;
-
-const LogoTitleWrapper = styled.div`
-  display: flex;
-  width: 100%;
-  align-items: center;
-  flex-direction: column;
-  font-family: 'Merienda', cursive;
-  color: #ffcc00;
-`;
-
-const LogoContainer = styled.div`
-  margin: 10px;
-`;
-
-const Button = styled.a`
-  background: #303eff;
-	color: white;
-	text-decoration: none;
-	font-family: 'Merienda', cursive;
-  text-align: center;
-  border-radius: 3px;
-	:visited{
-		color: white;
-	}
-	-moz-box-shadow:    2px 1px 6px black;
-  -webkit-box-shadow: 2px 1px 6px black;
-  box-shadow:         2px 1px 6px black;
-`;
-
-const SignInButtonWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  /* background: green; */
-  margin-top: 90px;
-  height: 30%;
-  justify-content: space-between;
-  /* height: */
-`;
-
-const SignInButton = styled(Button)`
-  height: 37px;
-  width: 50%;
-  
-`;
-
-const SecondParentSignInButton = styled(Button)`
-  
-`;
-
-const SignUpButtonWrapper = styled.div`
-  
-`;
-
-const SignUpButton = styled(Button)`
-  
-`;
-
-const BusinessSignUpButton = styled(Button)`
-  
-`;
