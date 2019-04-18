9c9efecd2185e27a4094828af5f92868c5863c4f
Michael Agard <mdagard@gmail.com>
2019-04-18 19:08:41 -0400

    add parentprofile import

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LP from './components/lp/LP';
 import Login from './components/login/Login';
 import ParentSignup from './components/signup/ParentSignup';
 import BusinessSignup from './components/signup/BusinessSignup';
+import ParentProfile from './components/profile/ParentProfile';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
           <Route path='/' component={LP} exact={true} />
           <Route path='/login' exact={true} render={(props) => <Login {...props} authenticate={() => this.authenticate()} />}  />
           <Route path='/signup' exact={true} component={ParentSignup}/>
+          <Route path='/parentprofile' exact={true} component={ParentProfile}/>
           <Route path='/business_signup' exact={true} component={BusinessSignup}/>
           <Redirect to='/' />
         </Switch>
