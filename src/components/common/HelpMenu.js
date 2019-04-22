02ed79025ce5d34d82678df5b509fb1356be070b
Michael Agard <mdagard@gmail.com>
2019-04-22 14:51:52 -0400

    cleaned up tabs

--- src/components/common/HelpMenu.js
+++ src/components/common/HelpMenu.js
@@ -3,26 +3,27 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const HelpMenu = () => {
-	
+
 	const HelpLink = styled(Link)`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 15%;
-	height: 8%;
-  background: #303eff;
-	color: white;
-	text-decoration: none;
-	font-family: 'Merienda', cursive;
-  text-align: center;
-  border-radius: 2px;
-	:visited{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 15%;
+		height: 8%;
+		background: #303eff;
 		color: white;
-	}
-	-moz-box-shadow:    2px 1px 6px black;
-  -webkit-box-shadow: 2px 1px 6px black;
-  box-shadow:         2px 1px 6px black;
-`;
+		text-decoration: none;
+		font-family: 'Merienda', cursive;
+		text-align: center;
+		border-radius: 2px;
+
+		:visited{
+			color: white;
+		}
+		-moz-box-shadow:    2px 1px 6px black;
+		-webkit-box-shadow: 2px 1px 6px black;
+		box-shadow:         2px 1px 6px black;
+	`;
 
 	return (
 			<HelpLink to='/help'>Help</HelpLink>
